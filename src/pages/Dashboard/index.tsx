ee0498e2b0408d4bd0345260cfc0d5db296c2cff
Junior580 <junior_silvapereira@hotmail.com>
2023-03-23 20:56:21 -0300

    ++api github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,80 @@
-import { Title, Form } from './style'
+import { Title, Form, Repositories, Error } from './style'
+import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
 import logoImg from '../../assets/logo.svg'
+import { useState, FormEvent } from 'react'
+
+interface IRepository {
+  full_name: string
+  description: string
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
+  const [repositories, setRepositories] = useState<IRepository[]>([])
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault()
+
+    if (!newRepo) {
+      setInputError('Digite autor/nome do repositório')
+      return
+    }
+
+    try {
+      const response = await api.get<IRepository>(`repos/${newRepo}`)
+
+      const repository = response.data
+
+      setRepositories([...repositories, repository])
+
+      setNewRepo('')
+
+      setInputError('')
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório')
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="github explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input type="text" placeholder="Digite aqui o nome do repisitório" />
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Digite aqui o nome do repisitório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="/">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
     </>
   )
 }
--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -43,3 +43,61 @@ export const Form = styled.form`
     }
   }
 `
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background-color: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`
